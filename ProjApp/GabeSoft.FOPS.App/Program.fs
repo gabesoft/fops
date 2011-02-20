63d5e0ebff4a4063421f0f01ebf95365e0282ba3
gabesoft <gabesoft@gmail.com>
2011-02-19 22:19:53 -0800

    console colors

--- ProjApp/GabeSoft.FOPS.App/Program.fs
+++ ProjApp/GabeSoft.FOPS.App/Program.fs
@@ -5,8 +5,9 @@ open GabeSoft.FOPS.Core
 [<EntryPoint>]
 let main (args) = 
   let log = new LogImpl()
+  let validatorLog = new LogImpl(ConsoleColor.DarkCyan)
   let opts = new Options(args)
-  let validator = new OptionsValidator(log)
+  let validator = new OptionsValidator(validatorLog)
   let server = new IOServer()
   let engine = new Engine(server, log)
 
--- ProjApp/GabeSoft.FOPS.Core/Log.fs
+++ ProjApp/GabeSoft.FOPS.Core/Log.fs
@@ -11,11 +11,22 @@ type Log =
   abstract member WarnWriter : TextWriter
   abstract member FailWriter : TextWriter
 
-type LogImpl () = 
+type LogImpl (?color) = 
+  let _color = match color with | Some c -> c | None -> Console.ForegroundColor
+  let changeColor color = 
+    let orig = Console.ForegroundColor
+    Console.ForegroundColor <- color
+    { new IDisposable with
+        member x.Dispose() = Console.ForegroundColor <- orig }
+  let writeln color = 
+    fun message -> 
+      use c = changeColor color
+      Console.WriteLine (message:string)   
+
   interface Log with
-    member x.Info message = Console.WriteLine(message)
-    member x.Warn message = Console.WriteLine(message)
-    member x.Fail message = Console.WriteLine(message)
+    member x.Info message = writeln _color message
+    member x.Warn message = writeln ConsoleColor.Yellow message
+    member x.Fail message = writeln ConsoleColor.Red message
     member x.InfoWriter = Console.Out
     member x.WarnWriter = Console.Out
     member x.FailWriter = Console.Out
